26657ad6c5c09746b5aba0d79f2dfb0893433256
Itzhak Levy <itzhaklevydev@gmail.com>
2025-07-23 13:08:23 +0300

    Create a type and an action creator function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("From index.js")
+// Create a type
+const BUY_CAKE = "BUY_CAKE";
+
+// Create an action creator function (a function that returns an action)
+function buyCake() {
+  return {
+    // Create an action (an object with a "type" property)
+    type: BUY_CAKE,
+    info: "First redux action"
+  };
+}
