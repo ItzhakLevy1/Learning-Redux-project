dda165db177841fd67304b4120cf124a3784aa38
Itzhak Levy <itzhaklevydev@gmail.com>
2025-07-29 14:30:59 +0300

    Add icecream module (slice)  with Redux setup (reducer, actions, types)

---
+++ REACT-REDUX-DEMO/react-redux-demo/src/redux/iceCream/iceCreamAction.js
@@ -0,0 +1,8 @@
+import { BUY_ICECREAM } from "./iceCreamType";
+
+// Create an action creator function
+export const buyIcecream = () => {
+  return {
+    type: BUY_ICECREAM,
+  };
+};
---
+++ REACT-REDUX-DEMO/react-redux-demo/src/redux/iceCream/iceCreamReducer.js
@@ -0,0 +1,22 @@
+// Import type
+import { BUY_ICECREAM } from "./iceCreamType";
+
+// Initial state
+const initialState = {
+  numOfIcecreams: 20,
+};
+
+// Reducer
+const icecreamReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIcecreams: state.numOfIcecreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default icecreamReducer;
