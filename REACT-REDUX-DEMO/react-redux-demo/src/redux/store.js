603b042442491730187329fd672611610e0e53cb
Itzhak Levy <itzhaklevydev@gmail.com>
2025-08-03 14:40:17 +0300

    Integrate Redux DevTools extension for state debugging
    - npm install @redux-devtools/extension
    - Edit store.js

--- REACT-REDUX-DEMO/react-redux-demo/src/redux/store.js
+++ REACT-REDUX-DEMO/react-redux-demo/src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger)); // Create a Redux store using the combined rootReducer as a parameter
+// Create a Redux store using the combined rootReducer as a parameter
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger)) // composeWithDevTools is used to enhance the store with Redux DevTools support, and logger middleware is applied for logging actions and state changes
+);
 
 export default store;
